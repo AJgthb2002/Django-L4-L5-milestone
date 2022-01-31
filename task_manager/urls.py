1991aca755bd6e5565e00ae0b85f73921a74d02d
Ananya Joshi <73184612+AJgthb2002@users.noreply.github.com>
2022-01-31 16:10:52 +0530

    Add files via upload

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,3 +1,18 @@
+"""task_manager URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from operator import index
 from django.contrib import admin
 from django.urls import path
@@ -34,10 +49,9 @@ def all_tasks_view(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tasks/', tasks_view),
-    path('addtask/', add_task_view),
-    path('delete_task/<int:index>/', delete_task_view),
+    path('add-task/', add_task_view),
+    path('delete-task/<int:index>/', delete_task_view),
     path('complete_task/<int:index>/', complete_task_view),
     path('completed_tasks/', completed_tasks),
     path('all_tasks/', all_tasks_view)
 ]
-
